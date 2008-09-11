458b61b454d2f93e73b9fb000874f1db544757e5
pascalbetz <pascal.betz@simplificator.com>
2008-09-11 11:03:53 +0200

    cleanup, removed some comments, bumped gem version

--- lib/flogger.rb
+++ lib/flogger.rb
@@ -1,8 +1,5 @@
-#$:.unshift File.dirname(__FILE__)     # For use/testing when no gem is installed
 require 'flogger/assertions'
 require 'test/unit'
 require 'rubygems'
 require 'flog'
 Test::Unit::TestCase.send(:include, Flogger::InstanceMethods)
-
-puts 'loaded flogger'
